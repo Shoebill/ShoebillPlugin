28e329ef6752150fc3baebb1f52e6ca0e45d3aed
Marvin Haschker <marvin@haschker.me>
2016-04-05 17:18:46 +0200

    Improve build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-cmake .
+CORE_COUNT=$(grep -c ^processor /proc/cpuinfo)
+
+rm -r build
+mkdir build
+cd build
+echo "Generating CMake Makefiles..."
+cmake .. -DCMAKE_BUILD_TYPE=Release
+echo "Producing ShoebillPlugin Release Executable..."
 make clean
-make
+make -j $CORE_COUNT
+echo "Build finished."
